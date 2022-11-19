3690a76bad69c9dbeaeaff9de0890f521d54cba4
Javier Valverde <soulsxhd@gmail.com>
2022-11-19 13:39:29 -0300

    Servicio de Añadir Cliente al trueque

--- controllers/trueques.js
+++ controllers/trueques.js
@@ -8,24 +8,25 @@ const Donacion  = require('../models/donación');
 const Reciclaje = require('../models/reciclaje');
 const Descuento = require('../models/descuento');
 const Usuario = require('../models/usuario');
+const Cliente = require('../models/cliente')
 
 const crearTrueque = async(req = request, res = response) => {
-// creamos el trueque
-const trueque = new Trueque();
+    // creamos el trueque
+    const trueque = new Trueque();
 
-//obtemenos el nombre completo del colaborador
-const { nombreCompleto } = req.body;
-//buscamos al colaborador dentro de la DB Usuarios
-const colaborador = await Usuario.findOne( nombreCompleto )    
-//le añadimos al trueque el id del colaborador
-trueque.idUsuario = colaborador;
+    //obtemenos el nombre completo del colaborador
+    const { nombreCompleto } = req.body;
+    //buscamos al colaborador dentro de la DB Usuarios
+     colaborador = await Usuario.findOne( nombreCompleto )    
+    //le añadimos al trueque el id del colaborador
+    trueque.idUsuario = colaborador;
 
-//conseguimos la fecha actual
-const hoy = new Date();
-//añadimos la fecha al trueque
-trueque.fecha = hoy;
-//guardamos el trueque
-trueque.save();
+    //conseguimos la fecha actual
+    const hoy = new Date();
+    //añadimos la fecha al trueque
+    trueque.fecha = hoy;
+    //guardamos el trueque
+    trueque.save();
     
 /*     const premium = new Premium();
     const segunda = new Segunda();
@@ -45,6 +46,19 @@ trueque.save();
     })
 }
 
+const agregarCliente = async(req = request, res = response) => {
+    const {idTrueque, rutCliente} = req.body;
+    const trueque = await Trueque.findById( idTrueque );
+    const cliente = await Cliente.findOne( { rut: rutCliente } );
+    trueque.idCliente = cliente._id;
+    trueque.save();
+
+    res.json({
+        msg: 'Funca'
+    })
+}
+
 module.exports = {
-    crearTrueque
+    crearTrueque,
+    agregarCliente
 }
--- routes/trueques.js
+++ routes/trueques.js
@@ -3,12 +3,14 @@ const { Router } = require('express');
 const { check } = require('express-validator');
 
 
-const { crearTrueque } = require('../controllers/trueques');
+const { crearTrueque, agregarCliente } = require('../controllers/trueques');
 
 const router = Router();
 
 
 router.post('/crearTrueque', crearTrueque );
+
+router.post('/agregarCliente',agregarCliente );
 
 
 
