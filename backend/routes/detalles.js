bd8e104643403bb432eb647823ef046e3416194a
Javier Valverde <soulsxhd@gmail.com>
2022-11-28 23:07:37 -0300

    detalles

--- backend/routes/detalles.js
+++ backend/routes/detalles.js
@@ -2,11 +2,6 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 
-
-const { 
-        validarCampos,
-    } = require('../middlewares')
-
 const { crearSegunda } = require('../controllers/detalles')
 
 const router = Router();
