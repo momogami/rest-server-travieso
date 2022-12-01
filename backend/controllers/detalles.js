e9211188b28c7cb6e76607b7b7def3d4097f386e
Javier Valverde <soulsxhd@gmail.com>
2022-12-01 18:02:06 -0300

    servicios para dropBox listos

--- backend/controllers/detalles.js
+++ backend/controllers/detalles.js
@@ -2,13 +2,87 @@ const { response, request } = require('express');
 
 const Detalle = require('../models/detalle')
 
-const crearSegunda = async(req, res = response) => {
-    const { talla, ropa, puntos } = req.body;
+const obtenerRopasTallasPremium = async( req, res = response ) => {
+   const premiums = await Detalle.find({ tipoRopa: 'PREMIUM' })
+   let listaTallas = [];
+   let listaRopas = [];
+   
+   premiums.forEach(premium => {
+        listaRopas.push(premium.ropa);
+        listaTallas.push(premium.talla)
+   });
+
+    listaRopas = listaRopas.filter((item,index)=>{
+    return listaRopas.indexOf(item) === index;
+    })
+
+    listaTallas = listaTallas.filter((item,index)=>{
+        return listaTallas.indexOf(item) === index;
+        })
     
+   res.json({
+       listaRopas:  listaRopas,
+       listaTallas: listaTallas 
+    })
+};
+
+const obtenerPrendasSegunda = async( req, res ) => {
+    const segundas = await Detalle.find({ tipoRopa: 'SEGUNDA' })
+    let listaPrendas = [];
+    
+    segundas.forEach(segunda => {
+         listaPrendas.push(segunda.ropa);
+    });
+
+    listaPrendas = listaPrendas.filter((item,index)=>{
+        return listaPrendas.indexOf(item) === index;
+     })
+     
+    res.json({
+        listaPrendas:  listaPrendas,
+     })
+};
+
+const obtenerPrendasDonacion = async( req, res ) => {
+    const donaciones = await Detalle.find({ tipoRopa: 'DONACION' })
+    let listaPrendas = [];
+    
+    donaciones.forEach(donacion => {
+         listaPrendas.push(donacion.ropa);
+    });
+
+    listaPrendas = listaPrendas.filter((item,index)=>{
+        return listaPrendas.indexOf(item) === index;
+     })
+     
+    res.json({
+        listaPrendas:  listaPrendas,
+     })
+};
+
+const obtenerPrendasDescuento = async( req, res ) => {
+    const descuentos = await Detalle.find({ tipoRopa: 'DESCUENTO' })
+    let listaPrendas = [];
+    
+    descuentos.forEach(descuento => {
+         listaPrendas.push(descuento.ropa);
+    });
+
+    listaPrendas = listaPrendas.filter((item,index)=>{
+        return listaPrendas.indexOf(item) === index;
+     })
+     
+    res.json({
+        listaPrendas:  listaPrendas,
+     })
 };
 
 
 
+
 module.exports = {
-    crearSegunda,
+    obtenerRopasTallasPremium,
+    obtenerPrendasSegunda,
+    obtenerPrendasDonacion,
+    obtenerPrendasDescuento
 }
--- backend/routes/detalles.js
+++ backend/routes/detalles.js
@@ -1,12 +1,20 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 
-const { post, crearSegunda } = require('../controllers/detalles');
+const {obtenerRopasTallasPremium,
+       obtenerPrendasSegunda,
+       obtenerPrendasDonacion,
+       obtenerPrendasDescuento } = require('../controllers/detalles');
 
 
 const router = Router();
 
-router.post('/crearSegunda', crearSegunda );
+router.get('/obtenerRopasTallasPremium', obtenerRopasTallasPremium );
     
+router.get('/obtenerPrendasSegunda', obtenerPrendasSegunda )
+
+router.get('/obtenerPrendasDonacion', obtenerPrendasDonacion )
+
+router.get('/obtenerPrendasDescuento', obtenerPrendasDescuento )
 
 module.exports = router;
