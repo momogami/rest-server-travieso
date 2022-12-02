564016fc86596a3fcc81969e1a8370c169074565
Javier Valverde <soulsxhd@gmail.com>
2022-12-02 13:17:39 -0300

    Controladores Detalle Trueques completo

--- backend/controllers/truequeDetalles.js
+++ backend/controllers/truequeDetalles.js
@@ -19,6 +19,8 @@ const agregarPremium = async(req, res = response) => {
         puntos: (detalle.puntos * cantidad)
     })
 
+    truequeDetalle.save()
+
     res.json({
         truequeDetalle: truequeDetalle._id,
         resumen:        resumen
@@ -42,6 +44,8 @@ const agregarSegunda = async(req, res = response) => {
         puntos: (detalle.puntos * cantidad)
     })
 
+    truequeDetalle.save()
+
     res.json({
         truequeDetalle: truequeDetalle._id,
         resumen:        resumen
@@ -49,9 +53,84 @@ const agregarSegunda = async(req, res = response) => {
     
 };
 
+const agregarDescuento = async(req, res = response) => {
+    
+    const { ropa, cantidad } = req.body;
+
+    const detalle =  await Detalle.findOne({ ropa: ropa })
+
+    const truequeDetalle = new TruequeDetalle({ idDetalle: detalle._id, cantidad: cantidad, puntosIniciales: (detalle.puntos * cantidad), kilos: null, deudaInicial: null })
+    
+    const resumen = ({
+        tipoTrueque: detalle.tipoRopa,
+        prenda:  ropa,
+        cantidad: cantidad,
+        deuda: "",
+        puntos: (detalle.puntos * cantidad)
+    })
+
+    truequeDetalle.save()
+
+    res.json({
+        truequeDetalle: truequeDetalle._id,
+        resumen:        resumen
+    })
+    
+};
+
+const agregarDonacion = async(req = request, res = response) => {
+    const { ropa, cantidad } = req.body;
+
+    const detalle =  await Detalle.findOne({ ropa: ropa })
+
+    const truequeDetalle = new TruequeDetalle({ idDetalle: detalle._id, cantidad: cantidad, puntosIniciales: (detalle.puntos * cantidad), kilos: null, deudaInicial: null })
+    
+    const resumen = ({
+        tipoTrueque: detalle.tipoRopa,
+        prenda:  ropa,
+        cantidad: cantidad,
+        deuda: "",
+        puntos: (detalle.puntos * cantidad)
+    })
+
+    truequeDetalle.save()
+
+    res.json({
+        truequeDetalle: truequeDetalle._id,
+        resumen:        resumen
+    })
+}
+
+const agregarReciclaje = async(req = request, res = response ) => {
+    const { cantidad } = req.body;
+
+    const detalle =  await Detalle.findOne({ tipoRopa: 'RECICLAJE' })
+
+    const truequeDetalle = new TruequeDetalle({ idDetalle: detalle._id, cantidad: cantidad, puntosIniciales: null, kilos: null, deudaInicial: (cantidad * detalle.deuda)  })
+    
+    const resumen = ({
+        tipoTrueque: detalle.tipoRopa,
+        prenda:  "",
+        cantidad: "",
+        deuda: ( cantidad * detalle.deuda ),
+        puntos: 0
+    })
+
+    truequeDetalle.save()
+
+    res.json({
+        truequeDetalle: truequeDetalle._id,
+        resumen:        resumen
+    })
+}
+
+
 
 
 module.exports = {
     agregarPremium,
-    agregarSegunda
+    agregarSegunda,
+    agregarDescuento,
+    agregarDonacion,
+    agregarReciclaje
 }
--- backend/routes/truequeDetalles.js
+++ backend/routes/truequeDetalles.js
@@ -1,7 +1,7 @@
 
 const { Router } = require('express');
 
-const { agregarPremium, agregarSegunda } = require('../controllers/truequeDetalles');
+const { agregarPremium, agregarSegunda, agregarDescuento, agregarDonacion, agregarReciclaje } = require('../controllers/truequeDetalles');
 
 const router = Router();
 
@@ -9,5 +9,11 @@ const router = Router();
 router.post( '/agregarPremium', agregarPremium );
 
 router.post( '/agregarSegunda', agregarSegunda )
+
+router.post( '/agregarDescuento', agregarDescuento )
+
+router.post( '/agregarDonacion', agregarDonacion )
+
+router.post( '/agregarReciclaje', agregarReciclaje)
 
 module.exports = router;
